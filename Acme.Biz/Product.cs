0b67ed17572e443939eb501d649aa4bf7810020f
Adrian Lisiecki <adrian.lisiecki@veinteractive.com>
2016-04-19 09:26:05 +0100

    Initial commit

---
+++ Acme.Biz/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory
+    /// </summary>
+    public class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("Created");
+        }
+
+        public Product(int productId, string productName, string description) : this()
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.Description = description;
+
+            Console.WriteLine("Another instance");
+        }
+
+        private string productName;
+        private string description;
+        private int productId;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        public string SayHello()
+        {
+            return "Hello " + ProductName + " (" + ProductId + "): " + Description;
+        }
+
+    }
+}
